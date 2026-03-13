bd569bb6430881f38b66abcd2aa23caaf7737f0c
UlrichFeuyan <ulrich.feuyan@gmail.com>
2026-03-13 15:56:17 +0100

    Implement and connecting auth routes

---
+++ backend/api/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from api.views import CreateUserView
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+
+
+app_name="api"
+urlpatterns = [
+    path("user/register/", CreateUserView.as_view(), name="register"),
+    path("token/", TokenObtainPairView.as_view(), name="get_token"),
+    path("token/refresh/", TokenRefreshView.as_view(), name="refresh"),
+]
