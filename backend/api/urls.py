216621f5a0351e1db61fab600eadcf1b787170f4
UlrichFeuyan <ulrich.feuyan@gmail.com>
2026-03-20 11:32:33 +0100

    Revert "implement jwt endpoints and init frontend project"
    This reverts commit c74f81444f28c058d4b2c37180c15251e3724441.

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from api.views import CreateUserView, NoteListCreate, NoteDelete
+from api.views import CreateUserView
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 
@@ -7,8 +7,5 @@ app_name="api"
 urlpatterns = [
     path("user/register/", CreateUserView.as_view(), name="register"),
     path("token/", TokenObtainPairView.as_view(), name="get_token"),
-    path("token/refresh/", TokenRefreshView.as_view(), name="refresdh"),
-    
-    path("notes/", NoteListCreate.as_view(), name="note-list"),
-    path("notes/delete/<int:pk>", NoteDelete.as_view(), name="note-delete")
+    path("token/refresh/", TokenRefreshView.as_view(), name="refresh"),
 ]
--- backend/api/views.py
+++ backend/api/views.py
@@ -1,9 +1,8 @@
+from django.shortcuts import render
 from django.contrib.auth.models import User
-from rest_framework.generics import CreateAPIView, ListCreateAPIView, DestroyAPIView
-from rest_framework.permissions import IsAuthenticated, AllowAny
+from rest_framework.generics import GenericAPIView, CreateAPIView
 from api.serializers import UserSerializer
-from api.models import Note
-from api.serializers import NoteSerializer
+from rest_framework.permissions import IsAuthenticated, AllowAny
 
 
 class CreateUserView(CreateAPIView):
@@ -11,44 +10,3 @@ class CreateUserView(CreateAPIView):
     serializer_class = UserSerializer
     permission_classes = [AllowAny]
 
-
-class NoteListCreate(ListCreateAPIView):
-    serializer_class = NoteSerializer
-    permission_classes = [IsAuthenticated] # Une requête ne peut obtenir réponse positive de cette vue que si elle inclus des informations d'authentification correctes
-
-    def get_queryset(self):
-        """surcharge la méthode get_queryset de ListCreateAPIView
-        Elles permet de limiter l'accès de cette fonction aux notes apartenant au User authentifier
-
-        Returns:
-            _type_: Note
-        """
-        user = self.request.user
-        return Note.objects.filter(author=user)
-    
-    def perform_create(self, serializer):
-        if serializer.is_valid():
-            serializer.save(author=self.request.user)
-        else:
-            print(serializer.errors)
-
-
-class NoteDelete(DestroyAPIView):
-    serializer_class = NoteSerializer
-    permission_classes = [IsAuthenticated]
-
-    def get_queryset(self):
-        """surcharge la méthode get_queryset de ListCreateAPIView
-        Elles permet de limiter l'accès de cette fonction aux notes apartenant au User authentifier
-
-        Returns:
-            _type_: Note
-        """
-        user = self.request.user
-        return Note.objects.filter(author=user)
-    
-    def perform_create(self, serializer):
-        if serializer.is_valid():
-            serializer.save(author=self.request.user)
-        else:
-            print(serializer.errors)
